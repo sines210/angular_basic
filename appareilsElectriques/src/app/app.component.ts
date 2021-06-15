9cc12f05e30dd0e2d18c355fce521cfeb4a5a909
sines210 <sines210@gmail.com>
2021-06-15 11:27:43 +0200

    angular_services

---
+++ appareilsElectriques/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
+
+@Component({
+  selector: 'app-root', 
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+
+export class AppComponent implements OnInit{
+
+  isAuth = false;
+
+lastUpdate: Promise<Date> = new Promise((resolve, reject) => {
+      const date = new Date();
+      setTimeout(() => {
+      resolve(date);
+      }, 2000);
+    }
+  )
+
+  appareils!:any[];
+
+
+  constructor (private appareilService : AppareilService ) {
+    setTimeout(() => {
+      this.isAuth = true
+    }, 4000);
+  }
+
+  onAllumer() {
+    this.appareilService.switchOnAll()
+  }
+
+  onEteindre() {
+    this.appareilService.switchOffAll()
+  }
+
+  ngOnInit() {
+    this.appareils = this .appareilService.appareils
+  }
+}
